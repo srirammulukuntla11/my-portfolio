b26aea944dde85db8792d6ac5908fc9b3573a4f4
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-08-07 00:07:07 +0530

    Connect frontend to Render backend

--- client/src/api/portfolioApi.js
+++ client/src/api/portfolioApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://my-portfolio-backend-mkp3.onrender.com/api",
 });
 
 export default api;
